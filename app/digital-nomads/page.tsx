eaf944252b3c205322117540221c47697a972acc
Mouhib <mouhibenns@gmail.com>
2024-03-13 19:59:44 +0100

    Fixed features bar in digital nomads page

--- app/digital-nomads/page.tsx
+++ app/digital-nomads/page.tsx
@@ -34,25 +34,39 @@ export default function Page() {
       >
         <h1 className="text-4xl font-bold text-white absolute">Djerba</h1>
       </div>
-      <div className="grid grid-cols-4 gap-4 bg-white rounded-lg p-8 -translate-y-8 drop-shadow-lg">
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 bg-white rounded-lg p-8 -translate-y-8 drop-shadow-lg text-center">
         {perks.map((perk, index) => (
           <div
             key={index}
-            className="rounded-lg flex flex-row space-x-4 relative"
+            className="rounded-lg flex flex-col items-center content-normal"
           >
-            <div className="p-4 bg-primary w-16 h-16 aspect-square rounded-full flex justify-center items-center">
+            <div className="bg-primary w-10 h-10 md:w-16 md:h-16 aspect-square rounded-full flex justify-center items-center">
               <perk.icon className="text-white" />
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col mt-4">
               <p className="text-lg">{perk.description}</p>
               <h2 className="text-2xl font-bold">{perk.title}</h2>
             </div>
           </div>
         ))}
-      </div>
+      </div>{" "}
       <div className="py-12 space-y-8 px-24">
         <div className="space-y-4">
-          <h1 className="text-4xl font-bold text-gray-900">Enjoy a pleasant climate all year round
+          <h1 className="text-2xl md:text-4xl font-bold text-gray-900">
+            Enjoy a pleasant climate all year round
+          </h1>
+          <p className="text-sm md:text-lg text-gray-600">
+            Djerba is a beautiful island located in the Gulf of Gabes, off the
+            coast of Tunisia. It is the largest island in North Africa, and the
+            fifth largest in the Mediterranean Sea. The island is known for its
+            beautiful beaches and pleasant climate all year round. The island is
+            also home to a number of historic sites, including the El Ghriba
+            Synagogue, which is one of the oldest synagogues in the world.
+          </p>
+        </div>
+        <div className="space-y-4">
+          <h1 className="text-4xl font-bold text-gray-900">
+            Enjoy a pleasant climate all year round
           </h1>
           <p className="text-lg text-gray-600">
             Djerba is a beautiful island located in the Gulf of Gabes, off the
@@ -64,19 +78,8 @@ export default function Page() {
           </p>
         </div>
         <div className="space-y-4">
-          <h1 className="text-4xl font-bold text-gray-900">Enjoy a pleasant climate all year round
-          </h1>
-          <p className="text-lg text-gray-600">
-            Djerba is a beautiful island located in the Gulf of Gabes, off the
-            coast of Tunisia. It is the largest island in North Africa, and the
-            fifth largest in the Mediterranean Sea. The island is known for its
-            beautiful beaches and pleasant climate all year round. The island is
-            also home to a number of historic sites, including the El Ghriba
-            Synagogue, which is one of the oldest synagogues in the world.
-          </p>
-        </div>
-        <div className="space-y-4">
-          <h1 className="text-4xl font-bold text-gray-900">Enjoy a pleasant climate all year round
+          <h1 className="text-4xl font-bold text-gray-900">
+            Enjoy a pleasant climate all year round
           </h1>
           <p className="text-lg text-gray-600">
             Djerba is a beautiful island located in the Gulf of Gabes, off the
