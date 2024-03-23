17f5f9ad97f8fa99c32bd0f9ba8463e90432280e
Mouhib <mouhibenns@gmail.com>
2024-03-23 17:22:06 +0100

    Secured actions with session

--- lib/actions/blog-actions.tsx
+++ lib/actions/blog-actions.tsx
@@ -6,6 +6,8 @@ import * as z from "zod";
 import slugify from "slugify";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/authOptions";
 
 export async function getAllBlogPosts() {
   const blogPosts = await prisma.blog.findMany({
@@ -28,60 +30,111 @@ export async function getBlogPostBySlug(slug: string) {
 }
 
 export async function deleteBlogPost(slug: string) {
-  await prisma.blog.delete({
-    where: {
-      slug: slug,
-    },
-  });
+  try {
+    const session = await getServerSession(authOptions);
 
-  console.log("Blog post deleted successfully");
-  revalidatePath("/blog");
-  redirect("/blog");
+    if (!session) {
+      throw new Error("Unauthorized");
+    }
+
+    if (!session.user || !session.user?.email) {
+      throw new Error("Unauthorized");
+    }
+
+    if (!session.user?.email.endsWith("@safertunisia.com")) {
+      throw new Error("Unauthorized");
+    }
+
+    await prisma.blog.delete({
+      where: {
+        slug: slug,
+      },
+    });
+
+    console.log("Blog post deleted successfully");
+    revalidatePath("/blog");
+    redirect("/blog");
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function updateBlogPost(
   slug: string,
   values: z.infer<typeof newBlogPostFormSchema>
 ) {
-  await prisma.blog.update({
-    where: {
-      slug: slug,
-    },
-    data: {
-      title: values.title,
-      slug: slugify(values.title, {
-        lower: true,
-        strict: true,
-      }),
-      content: values.content,
-      description: values.description,
-      imageUrl: values.imageURL,
-      authorId: values.author,
-    },
-  });
+  try {
+    const session = await getServerSession(authOptions);
 
-  console.log("Blog post updated successfully");
-  revalidatePath(`/blog`);
-  redirect(`/blog`);
+    if (!session) {
+      throw new Error("Unauthorized");
+    }
+
+    if (!session.user || !session.user?.email) {
+      throw new Error("Unauthorized");
+    }
+
+    if (!session.user?.email.endsWith("@safertunisia.com")) {
+      throw new Error("Unauthorized");
+    }
+
+    await prisma.blog.update({
+      where: {
+        slug: slug,
+      },
+      data: {
+        title: values.title,
+        slug: slugify(values.title, {
+          lower: true,
+          strict: true,
+        }),
+        content: values.content,
+        description: values.description,
+        imageUrl: values.imageURL,
+        authorId: values.author,
+      },
+    });
+  
+    console.log("Blog post updated successfully");
+    revalidatePath(`/blog`);
+    redirect(`/blog`);
+  } catch (error) {
+    console.error(error);
+  }
 }     
 
 export async function createBlogPost(
   values: z.infer<typeof newBlogPostFormSchema>
 ) {
-  await prisma.blog.create({
-    data: {
-      slug: slugify(values.title, {
-        lower: true,
-        strict: true,
-      }), 
-      title: values.title,
-      description: values.description,
-      imageUrl: values.imageURL,
-      authorId: values.author,
-      content: values.content,
-    },
-  });
+  try {
+    const session = await getServerSession(authOptions);
 
-  console.log("Blog post created successfully");
-  redirect("/blog");
+    if (!session) {
+      throw new Error("Unauthorized");
+    }
+
+    if (!session.user || !session.user?.email) {
+      throw new Error("Unauthorized");
+    }
+
+    if (!session.user?.email.endsWith("@safertunisia.com")) {
+      throw new Error("Unauthorized");
+    }
+
+    await prisma.blog.create({
+      data: {
+        slug: slugify(values.title, {
+          lower: true,
+          strict: true,
+        }), 
+        title: values.title,
+        description: values.description,
+        imageUrl: values.imageURL,
+        authorId: values.author,
+        content: values.content,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
