e6abcccab5677f94f3d2267035ffa8755bcae0cc
Mouhib Ben nasser <48995160+meowhib@users.noreply.github.com>
2024-04-30 08:27:15 +0000

    Improved schema

--- next.config.mjs
+++ next.config.mjs
@@ -1,32 +1,32 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'www.safertunisia.com',
-          },
-          {
-            protocol: 'http',
-            hostname: 'www.safertunisia.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'safertunisia.com',
-          },
-          {
-            protocol: 'https',
-            hostname: 'supabase.com',
-            pathname: '/dashboard/project/xubahczbamolpabdcyfm/storage/buckets/safertunisia/**'
-          },
-          {
-            protocol: 'https',
-            hostname: 'images.unsplash.com',
-            pathname: '/**'
-          },
-        ],
-      },    
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.safertunisia.com",
+      },
+      {
+        protocol: "http",
+        hostname: "www.safertunisia.com",
+      },
+      {
+        protocol: "https",
+        hostname: "safertunisia.com",
+      },
+      {
+        protocol: "https",
+        hostname: "xubahczbamolpabdcyfm.supabase.co",
+        pathname: "/storage/v1/object/public/safertunisia/**",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
