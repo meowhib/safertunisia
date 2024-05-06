8fe342ee8c4828410caa327bf7b21e57dd9ce4d9
Mouhib Ben nasser <48995160+meowhib@users.noreply.github.com>
2024-05-06 10:30:50 +0000

    Removed console logs for better security

--- lib/authOptions.ts
+++ lib/authOptions.ts
@@ -1,66 +1,64 @@
-import { type NextAuthOptions } from 'next-auth'
-import CredentialsProvider from "next-auth/providers/credentials"
-import prisma from "@/lib/prisma"
+import { type NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import prisma from "@/lib/prisma";
 
 export const authOptions: NextAuthOptions = {
   session: {
-    strategy: 'jwt'
+    strategy: "jwt",
   },
   providers: [
     CredentialsProvider({
-      name: 'Sign in',
+      name: "Sign in",
       credentials: {
         email: {
-          label: 'Email',
-          type: 'email',
-          placeholder: 'hello@example.com'
+          label: "Email",
+          type: "email",
+          placeholder: "hello@example.com",
         },
-        password: { label: 'Password', type: 'password' }
+        password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
         if (!credentials?.email || !credentials.password) {
-          return null
+          return null;
         }
 
         const user = await prisma.author.findUnique({
           where: {
-            email: credentials.email
-          }
-        })
+            email: credentials.email,
+          },
+        });
 
         if (!user) {
-          return null
+          return null;
         }
 
         return {
-          id: user.id + '',
+          id: user.id + "",
           email: user.email,
           name: user.name,
-        }
-      }
-    })
+        };
+      },
+    }),
   ],
   callbacks: {
     session: ({ session, token }) => {
-      console.log('Session Callback', { session, token })
       return {
         ...session,
         user: {
           ...session.user,
           id: token.id,
-        }
-      }
+        },
+      };
     },
     jwt: ({ token, user }) => {
-      console.log('JWT Callback', { token, user })
       if (user) {
-        const u = user as unknown as any
+        const u = user as unknown as any;
         return {
           ...token,
           id: u.id,
-        }
+        };
       }
-      return token
-    }
-  }
-}
+      return token;
+    },
+  },
+};
